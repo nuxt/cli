f1909c8d0d0c07f9bd0cb295a6620754bc70593c
Pooya Parsa <pooya@pi0.io>
2023-02-15 14:17:52 +0100

    feat: basic cli update checker

---
+++ src/utils/update.ts
@@ -0,0 +1,27 @@
+import { name as pkgName, version as currentVersion } from '../../package.json'
+import { $fetch } from 'ofetch'
+import { cyan, green, yellow } from 'colorette'
+import boxen from 'boxen'
+import * as semver from 'semver'
+
+export async function checkForUpdates() {
+  const { version: latestVersion = '' } = await $fetch(`https://registry.npmjs.org/${pkgName}/latest`)
+  if (!latestVersion) {
+    return
+  }
+  if (semver.gt(latestVersion, currentVersion)) {
+    console.log(boxen(`
+A new version of Nuxt CLI is available: ${green(latestVersion)}
+You are currently using ${yellow(currentVersion)}
+To update: ${cyan(`npm install -g ${pkgName}`)}
+      `.trim(),
+      {
+        padding: 1,
+        margin: 1,
+        borderStyle: 'round',
+        title: 'CLI Update Available',
+        borderColor: 'yellow'
+      }
+    ))
+  }
+}
