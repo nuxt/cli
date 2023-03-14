9b99a2adf27a877da4a54bed411afb341e7119a4
Pooya Parsa <pooya@pi0.io>
2023-03-14 15:25:19 +0100

    add main entry with bw compat

--- build.config.ts
+++ build.config.ts
@@ -17,6 +17,7 @@ export default defineBuildConfig({
     '@nuxt/schema',
     '@nuxt/test-utils',
     'fsevents',
+    'citty',
     // TODO: Fix rollup/unbuild issue
     'node:url',
     'node:buffer',
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-export * from './run'
+import { runCommand as _runCommand } from 'citty'
+import { commands } from './commands'
+
+// Backward compatibility
+export async function runCommand(
+  commandName: string,
+  argv = process.argv.slice(2)
+) {
+  const subCommand = (commands as any)[commandName]
+  if (!subCommand) {
+    throw new Error(`Unknown command: ${commandName}`)
+  }
+  await _runCommand(subCommand, {
+    rawArgs: argv,
+  })
+}
