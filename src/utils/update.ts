e9c9b49e8b4836807c056b46512471956d1d1dc5
Pooya Parsa <pooya@pi0.io>
2023-06-19 12:13:25 +0200

    fix: remove boxen dependency

--- src/utils/update.ts
+++ src/utils/update.ts
@@ -1,7 +1,6 @@
 import { name as pkgName, version as currentVersion } from '../../package.json'
 import { $fetch } from 'ofetch'
-import { cyan, green, yellow, underline } from 'colorette'
-import boxen from 'boxen'
+import { cyan, green, yellow, underline, gray } from 'colorette'
 import * as semver from 'semver'
 
 export async function checkForUpdates() {
@@ -15,25 +14,17 @@ export async function checkForUpdates() {
     return
   }
   if (semver.gt(latestVersion, currentVersion, { loose: true })) {
-    const changelogVersionHash = 'v' + latestVersion.replace(/\./g, '')
-    const changelogURL = `https://github.com/nuxt/cli/blob/main/CHANGELOG.md#${changelogVersionHash}`
+    const changelogURL = `https://github.com/nuxt/cli/releases/tag/v${latestVersion}`
     console.log(
-      boxen(
-        `
-A new version of Nuxt CLI is available: ${green(latestVersion)}
-You are currently using ${yellow(currentVersion)}
-Changelog: ${underline(cyan(changelogURL))}
+      `
+${gray('    --------------------------------------------------------------')}
+    A new version of Nuxt CLI is available: ${green(latestVersion)}
+    You are currently using ${yellow(currentVersion)}
+    Release notes: ${underline(cyan(changelogURL))}
 
-To update: ${cyan(`npm install -g ${pkgName}`)}
-      `.trim(),
-        {
-          padding: 1,
-          margin: 1,
-          borderStyle: 'round',
-          title: 'CLI Update Available',
-          borderColor: 'yellow',
-        }
-      )
+    To update: ${cyan(`npm install -g ${pkgName}`)}
+${gray('    --------------------------------------------------------------')}
+      `.trim() + '\n'
     )
   }
 }
