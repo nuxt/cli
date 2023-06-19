60064ece48e389e0eeafa8eeef06e22984c87577
Pooya Parsa <pooya@pi0.io>
2023-06-19 11:50:34 +0200

    refactor: move cli to standalone package

--- build.config.ts
+++ build.config.ts
@@ -5,27 +5,21 @@ export default defineBuildConfig({
   rollup: {
     inlineDependencies: true,
     resolve: {
-      exportConditions: ['production', 'node'] as any
-    }
+      exportConditions: ['production', 'node'] as any,
+    },
   },
-  entries: [
-    'src/cli',
-    'src/cli-run',
-    'src/cli-wrapper',
-    'src/index'
-  ],
+  entries: ['src/cli', 'src/cli-run', 'src/cli-wrapper', 'src/index'],
   externals: [
     '@nuxt/kit',
     '@nuxt/schema',
     '@nuxt/test-utils',
     'fsevents',
-    // TODO: Fix rollup/unbuild issue
     'node:url',
     'node:buffer',
     'node:path',
     'node:child_process',
     'node:process',
     'node:path',
-    'node:os'
-  ]
+    'node:os',
+  ],
 })
