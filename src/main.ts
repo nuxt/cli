816b5bcdf569fbc4ce58f05d9e30106a2691fa88
Pooya Parsa <pooya@pi0.io>
2023-08-30 13:36:45 +0200

    fix: add cli version and name in usage

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { defineCommand } from 'citty'
 import { commands } from './commands'
 import { setupGlobalConsole } from './utils/console'
 import { checkEngines } from './utils/engines'
+import { name, version, description } from '../package.json'
+
 // import { checkForUpdates } from './utils/update'
 
 export const main = defineCommand({
   meta: {
-    name: 'nuxi',
-    description: 'Nuxt CLI',
+    name,
+    version,
+    description,
   },
   subCommands: commands,
   async setup(ctx) {
