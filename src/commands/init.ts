fe8ef7cffab1da38ce5fe2cb337e8b9233d05244
murisceman <murisceman@gmail.com>
2023-09-19 00:10:18 +0200

    feat(init): support `--package-manager`  (#97)
    * feat: add `manager` command
    
    * rename arg
    
    * simplify types
    
    ---------
    
    Co-authored-by: Pooya Parsa <pooya@pi0.io>

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -52,6 +52,10 @@ export default defineCommand({
       type: 'boolean',
       description: 'Start shell after installation in project directory',
     },
+    packageManager: {
+      type: 'string',
+      description: 'Package manager choice (npm, pnpm, yarn, bun)',
+    },
   },
   async run(ctx) {
     const cwd = resolve(ctx.args.cwd || '.')
@@ -84,14 +88,22 @@ export default defineCommand({
       process.exit(1)
     }
 
-    // Prompt user to select package manager
-    const selectedPackageManager = await consola.prompt<{
-      type: 'select'
-      options: PackageManagerName[]
-    }>('Which package manager would you like to use?', {
-      type: 'select',
-      options: ['npm', 'pnpm', 'yarn', 'bun'],
-    })
+    // Resolve package manager
+    const packageManagerOptions: PackageManagerName[] = [
+      'npm',
+      'pnpm',
+      'yarn',
+      'bun',
+    ]
+    const packageManagerArg = ctx.args.packageManager as PackageManagerName
+    const selectedPackageManager = packageManagerOptions.includes(
+      packageManagerArg,
+    )
+      ? packageManagerArg
+      : await consola.prompt('Which package manager would you like to use?', {
+          type: 'select',
+          options: packageManagerOptions,
+        })
 
     // Get relative project path
     const relativeProjectPath = relative(process.cwd(), template.dir)
