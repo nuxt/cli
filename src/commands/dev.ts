nuxt/cli

commit b62e8e382d1418c7672b9280540d6690f7d75c2f

fix: make dev proxy url available for `listen` hook (#206)

src/commands/dev.ts
@@ -15,10 +15,9 @@ import { sharedArgs, legacyRootDirArgs } from './_shared'
 
 import type { HTTPSOptions, ListenOptions } from 'listhen'
 import type { ChildProcess } from 'node:child_process'
-import type { DevChildContext } from './dev-child'
 import type { NuxtOptions } from '@nuxt/schema'
 import type { IncomingMessage, ServerResponse } from 'node:http'
-import type { NuxtDevIPCMessage } from '../utils/dev'
+import type { NuxtDevContext, NuxtDevIPCMessage } from '../utils/dev'
 
 const forkSupported = !isBun && !isTest
 
@@ -80,7 +79,13 @@ const command = defineCommand({
         clear: ctx.args.clear,
         dotenv: !!ctx.args.dotenv,
         loadingTemplate: nuxtOptions.devServer.loadingTemplate,
-        https: devProxy.listener.https,
+        devContext: {
+          proxy: {
+            https: devProxy.listener.https,
+            url: devProxy.listener.url,
+            urls: await devProxy.listener.getURLs(),
+          },
+        },
       })
       devProxy.setAddress(devServer.listener.url)
       await devServer.init()
@@ -172,11 +177,13 @@ async function _startSubprocess(devProxy: DevProxy) {
         ].filter(Boolean) as string[],
         env: {
           ...process.env,
-          __NUXT_DEV_PROXY__: JSON.stringify({
-            url: devProxy.listener.url,
-            urls: await devProxy.listener.getURLs(),
-            https: devProxy.listener.https,
-          } satisfies DevChildContext),
+          __NUXT_DEV__: JSON.stringify({
+            proxy: {
+              url: devProxy.listener.url,
+              urls: await devProxy.listener.getURLs(),
+              https: devProxy.listener.https,
+            },
+          } satisfies NuxtDevContext),
         },
       },
     )

src/utils/dev.ts
@@ -5,7 +5,7 @@ import chokidar from 'chokidar'
 import { consola } from 'consola'
 import { debounce } from 'perfect-debounce'
 import { toNodeListener } from 'h3'
-import { HTTPSOptions, listen, Listener } from 'listhen'
+import { HTTPSOptions, ListenURL, listen, Listener } from 'listhen'
 import type { Nuxt, NuxtConfig } from '@nuxt/schema'
 import { loadKit } from '../utils/kit'
 import { loadNuxtManifest, writeNuxtManifest } from '../utils/nuxt'
@@ -17,24 +17,48 @@ export type NuxtDevIPCMessage =
   | { type: 'nuxt:internal:dev:loading'; message: string }
   | { type: 'nuxt:internal:dev:restart' }
 
+export interface NuxtDevContext {
+  proxy?: {
+    url?: string
+    urls?: ListenURL[]
+    https?: boolean | HTTPSOptions
+  }
+}
+
 export interface NuxtDevServerOptions {
   cwd: string
   logLevel: 'silent' | 'info' | 'verbose'
   dotenv: boolean
   clear: boolean
   overrides: NuxtConfig
-  https?: boolean | HTTPSOptions
   port?: string | number
   loadingTemplate?: ({ loading }: { loading: string }) => string
+  devContext: NuxtDevContext
 }
 
 export async function createNuxtDevServer(options: NuxtDevServerOptions) {
+  // Initialize dev server
   const devServer = new NuxtDevServer(options)
+
+  // Attach internal listener
   devServer.listener = await listen(devServer.handler, {
     port: options.port ?? 0,
     hostname: '127.0.0.1',
     showURL: false,
   })
+
+  // Merge interface with public context
+  if (options.devContext.proxy?.url) {
+    devServer.listener.url = options.devContext.proxy.url
+  }
+  if (options.devContext.proxy?.urls) {
+    const _getURLs = devServer.listener.getURLs.bind(devServer.listener)
+    devServer.listener.getURLs = async () =>
+      Array.from(
+        new Set([...options.devContext.proxy!.urls!, ...(await _getURLs())]),
+      )
+  }
+
   return devServer
 }
 
@@ -213,9 +237,8 @@ class NuxtDevServer extends EventEmitter {
     this._currentNuxt.options.devServer.url = `http://${
       addr.address.includes(':') ? `[${addr.address}]` : addr.address
     }:${addr.port}/`
-    this._currentNuxt.options.devServer.https = this.options.https as
-      | boolean
-      | { key: string; cert: string }
+    this._currentNuxt.options.devServer.https = this.options.devContext.proxy
+      ?.https as boolean | { key: string; cert: string }
 
     await Promise.all([
       kit.writeTypes(this._currentNuxt).catch(console.error),

src/commands/dev-child.ts
@@ -4,14 +4,11 @@ import { overrideEnv } from '../utils/env'
 import { defineCommand } from 'citty'
 import { sharedArgs, legacyRootDirArgs } from './_shared'
 import { isTest } from 'std-env'
-import { NuxtDevIPCMessage, createNuxtDevServer } from '../utils/dev'
-import type { ListenURL, HTTPSOptions } from 'listhen'
-
-export type DevChildContext = {
-  url?: string
-  urls?: ListenURL[]
-  https?: boolean | HTTPSOptions
-}
+import {
+  NuxtDevContext,
+  NuxtDevIPCMessage,
+  createNuxtDevServer,
+} from '../utils/dev'
 
 export default defineCommand({
   meta: {
@@ -36,9 +33,9 @@ export default defineCommand({
     overrideEnv('development')
     const cwd = resolve(ctx.args.cwd || ctx.args.rootDir || '.')
 
-    // Get host info
-    const devProxyOptions: DevChildContext =
-      JSON.parse(process.env.__NUXT_DEV_PROXY__ || 'null') || {}
+    // Get dev context info
+    const devContext: NuxtDevContext =
+      JSON.parse(process.env.__NUXT_DEV__ || 'null') || {}
 
     // Init Nuxt dev
     const nuxtDev = await createNuxtDevServer({
@@ -47,8 +44,8 @@ export default defineCommand({
       logLevel: ctx.args.logLevel as 'silent' | 'info' | 'verbose',
       clear: !!ctx.args.clear,
       dotenv: !!ctx.args.dotenv,
-      https: devProxyOptions.https,
       port: process.env._PORT ?? undefined,
+      devContext,
     })
 
     // IPC Hooks

commit 1316da60183c2bafd3f045da66c795a4bbb9ed49

fix: use internal dev server url for dev proxy
hotfix for a regression from #206 with no-fork mode

src/commands/dev.ts
@@ -87,7 +87,8 @@ const command = defineCommand({
           },
         },
       })
-      devProxy.setAddress(devServer.listener.url)
+      // @ts-expect-error
+      devProxy.setAddress(devServer.listener._url || devServer.listener.url)
       await devServer.init()
     }
   },

src/utils/dev.ts
@@ -48,6 +48,8 @@ export async function createNuxtDevServer(options: NuxtDevServerOptions) {
   })
 
   // Merge interface with public context
+  // @ts-expect-error
+  devServer.listener._url = devServer.listener.url
   if (options.devContext.proxy?.url) {
     devServer.listener.url = options.devContext.proxy.url
   }