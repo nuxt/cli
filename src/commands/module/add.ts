aee7f8c9a0ce846155206bcd53d89448009b5801
rohrig <richard.t.rohrig@gmail.com>
2023-09-26 23:42:21 +0200

    feat: add multiple modules at once

--- src/commands/module/add.ts
+++ src/commands/module/add.ts
@@ -1,117 +1,123 @@
-import {resolve} from 'pathe'
-import {defineCommand} from 'citty'
-import {sharedArgs} from '../_shared'
-import {existsSync} from 'node:fs'
-import {loadFile, writeFile, parseModule, ProxifiedModule} from 'magicast'
+import { resolve } from 'pathe'
+import { defineCommand } from 'citty'
+import { sharedArgs } from '../_shared'
+import { existsSync } from 'node:fs'
+import { loadFile, writeFile, parseModule, ProxifiedModule } from 'magicast'
 import consola from 'consola'
-import {addDependency} from 'nypm'
+import { addDependency } from 'nypm'
 import {
-    NuxtModule,
-    checkNuxtCompatibility,
-    fetchModules,
-    getNuxtVersion,
+  NuxtModule,
+  checkNuxtCompatibility,
+  fetchModules,
+  getNuxtVersion,
 } from './_utils'
-import {satisfies} from 'semver'
-import {colors} from 'consola/utils'
+import { satisfies } from 'semver'
+import { colors } from 'consola/utils'
 
 export default defineCommand({
-    meta: {
-        name: 'module',
-        description: 'Manage Nuxt Modules',
+  meta: {
+    name: 'module',
+    description: 'Manage Nuxt Modules',
+  },
+  args: {
+    ...sharedArgs,
+    moduleName: {
+      type: 'positional',
+      description: 'Module name',
     },
-    args: {
-        ...sharedArgs,
-        moduleName: {
-            type: 'positional',
-            description: 'Module name',
-        },
-        skipInstall: {
-            type: 'boolean',
-            description: 'Skip npm install',
-        },
-        skipConfig: {
-            type: 'boolean',
-            description: 'Skip nuxt.config.ts update',
-        },
+    skipInstall: {
+      type: 'boolean',
+      description: 'Skip npm install',
     },
-    async setup(ctx) {
-        const cwd = resolve(ctx.args.cwd || '.')
-        const modules = ctx.args._
+    skipConfig: {
+      type: 'boolean',
+      description: 'Skip nuxt.config.ts update',
+    },
+  },
+  async setup(ctx) {
+    const cwd = resolve(ctx.args.cwd || '.')
+    const modules = ctx.args._
 
-        for (const moduleName of modules) {
-            const r = await resolveModule(ctx.args.moduleName, cwd)
-            if (r === false) {
-                return
-            }
-            await addModule(ctx, cwd, moduleName)
-        }
+    for (const moduleName of modules) {
+      const isResolved = await resolveModule(moduleName, cwd)
+      if (isResolved === false) {
+        return
+      }
+      consola.info(`${moduleName} has been resolved. Adding module...`)
+      await addModule(moduleName, ctx.args.skipInstall, ctx.args.skipConfig, cwd)
     }
+  }
 })
 
 // -- Internal Utils --
-async function addModule(ctx, cwd: string, npmPackage: string) {
-    // Add npm dependency
-    if (!ctx.args.skipInstall) {
-        consola.info(`Installing dev dependency \`${npmPackage}\``)
-        await addDependency(npmPackage, {cwd, dev: true}).catch((err) => {
-            consola.error(err)
-            consola.error(
-                `Please manually install \`${npmPackage}\` as a dev dependency`,
-            )
-        })
-    }
+async function addModule(
+  npmPackage: string,
+  skipInstall: boolean,
+  skipConfig: boolean,
+  cwd: string
+) {
+  // Add npm dependency
+  if (!skipInstall) {
+    consola.info(`Installing dev dependency \`${npmPackage}\``)
+    await addDependency(npmPackage, { cwd, dev: true }).catch((err) => {
+      consola.error(err)
+      consola.error(
+        `Please manually install \`${npmPackage}\` as a dev dependency`,
+      )
+    })
+  }
 
-    // Update nuxt.config.ts
-    if (!ctx.args.skipConfig) {
-        await updateNuxtConfig(cwd, (config) => {
-            if (!config.modules) {
-                config.modules = []
-            }
-            for (let i = 0; i < config.modules.length; i++) {
-                if (config.modules[i] === npmPackage) {
-                    consola.info(`\`${npmPackage}\` is already in the \`modules\``)
-                    return
-                }
-            }
-            consola.info(`Adding \`${npmPackage}\` to the \`modules\``)
-            config.modules.push(npmPackage)
-        }).catch((err) => {
-            consola.error(err)
-            consola.error(
-                `Please manually add \`${npmPackage}\` to the \`modules\` in \`nuxt.config.ts\``,
-            )
-        })
-    }
+  // Update nuxt.config.ts
+  if (!skipConfig) {
+    await updateNuxtConfig(cwd, (config) => {
+      if (!config.modules) {
+        config.modules = []
+      }
+      for (let i = 0; i < config.modules.length; i++) {
+        if (config.modules[i] === npmPackage) {
+          consola.info(`\`${npmPackage}\` is already in the \`modules\``)
+          return
+        }
+      }
+      consola.info(`Adding \`${npmPackage}\` to the \`modules\``)
+      config.modules.push(npmPackage)
+    }).catch((err) => {
+      consola.error(err)
+      consola.error(
+        `Please manually add \`${npmPackage}\` to the \`modules\` in \`nuxt.config.ts\``,
+      )
+    })
+  }
 }
 
 async function updateNuxtConfig(
-    rootDir: string,
-    update: (config: any) => void,
+  rootDir: string,
+  update: (config: any) => void,
 ) {
-    let _module: ProxifiedModule
-    const nuxtConfigFile = resolve(rootDir, 'nuxt.config.ts')
-    if (existsSync(nuxtConfigFile)) {
-        consola.info('Updating `nuxt.config.ts`')
-        _module = await loadFile(nuxtConfigFile)
-    } else {
-        consola.info('Creating `nuxt.config.ts`')
-        _module = parseModule(getDefaultNuxtConfig())
-    }
-    const defaultExport = _module.exports.default
-    if (!defaultExport) {
-        throw new Error('`nuxt.config.ts` does not have a default export!')
-    }
-    if (defaultExport.$type === 'function-call') {
-        update(defaultExport.$args[0])
-    } else {
-        update(defaultExport)
-    }
-    await writeFile(_module as any, nuxtConfigFile)
-    consola.success('`nuxt.config.ts` updated')
+  let _module: ProxifiedModule
+  const nuxtConfigFile = resolve(rootDir, 'nuxt.config.ts')
+  if (existsSync(nuxtConfigFile)) {
+    consola.info('Updating `nuxt.config.ts`')
+    _module = await loadFile(nuxtConfigFile)
+  } else {
+    consola.info('Creating `nuxt.config.ts`')
+    _module = parseModule(getDefaultNuxtConfig())
+  }
+  const defaultExport = _module.exports.default
+  if (!defaultExport) {
+    throw new Error('`nuxt.config.ts` does not have a default export!')
+  }
+  if (defaultExport.$type === 'function-call') {
+    update(defaultExport.$args[0])
+  } else {
+    update(defaultExport)
+  }
+  await writeFile(_module as any, nuxtConfigFile)
+  consola.success('`nuxt.config.ts` updated')
 }
 
 function getDefaultNuxtConfig() {
-    return `
+  return `
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: []
@@ -120,94 +126,94 @@ export default defineNuxtConfig({
 
 // Based on https://github.com/dword-design/package-name-regex
 const packageRegex =
-    /^(@[a-z0-9-~][a-z0-9-._~]*\/)?([a-z0-9-~][a-z0-9-._~]*)(@[^@]+)?$/
+  /^(@[a-z0-9-~][a-z0-9-._~]*\/)?([a-z0-9-~][a-z0-9-._~]*)(@[^@]+)?$/
 
 async function resolveModule(
-    moduleName: string,
-    cwd: string,
+  moduleName: string,
+  cwd: string,
 ): Promise<
-    | false
-    | {
+  | false
+  | {
     nuxtModule?: NuxtModule
     pkg: string
     pkgName: string
     pkgVersion: string
-}
+  }
 > {
-    let pkgName = moduleName
-    let pkgVersion: string | undefined
+  let pkgName = moduleName
+  let pkgVersion: string | undefined
 
-    const reMatch = moduleName.match(packageRegex)
-    if (reMatch) {
-        if (reMatch[3]) {
-            pkgName = `${reMatch[1] || ''}${reMatch[2] || ''}`
-            pkgVersion = reMatch[3].slice(1)
-        }
-    } else {
-        consola.error(`Invalid package name \`${pkgName}\`.`)
+  const reMatch = moduleName.match(packageRegex)
+  if (reMatch) {
+    if (reMatch[3]) {
+      pkgName = `${reMatch[1] || ''}${reMatch[2] || ''}`
+      pkgVersion = reMatch[3].slice(1)
+    }
+  } else {
+    consola.error(`Invalid package name \`${pkgName}\`.`)
+    return false
+  }
+
+  const modulesDB = await fetchModules().catch((err) => {
+    consola.warn('Cannot search in the Nuxt Modules database: ' + err)
+    return []
+  })
+
+  const matchedModule = modulesDB.find(
+    (module) => module.name === moduleName || module.npm === pkgName,
+  )
+
+  if (matchedModule?.npm) {
+    pkgName = matchedModule.npm
+  }
+
+  if (matchedModule && matchedModule.compatibility.nuxt) {
+    // Get local Nuxt version
+    const nuxtVersion = await getNuxtVersion(cwd)
+
+    // Check for Module Compatibility
+    if (!checkNuxtCompatibility(matchedModule, nuxtVersion)) {
+      consola.warn(
+        `The module \`${pkgName}\` is not compatible with Nuxt \`${nuxtVersion}\` (requires \`${matchedModule.compatibility.nuxt}\`)`,
+      )
+      const shouldContinue = await consola.prompt(
+        'Do you want to continue installing incompatible version?',
+        {
+          type: 'confirm',
+          initial: false,
+        },
+      )
+      if (shouldContinue !== true) {
         return false
+      }
     }
 
-    const modulesDB = await fetchModules().catch((err) => {
-        consola.warn('Cannot search in the Nuxt Modules database: ' + err)
-        return []
-    })
-
-    const matchedModule = modulesDB.find(
-        (module) => module.name === moduleName || module.npm === pkgName,
-    )
-
-    if (matchedModule?.npm) {
-        pkgName = matchedModule.npm
-    }
-
-    if (matchedModule && matchedModule.compatibility.nuxt) {
-        // Get local Nuxt version
-        const nuxtVersion = await getNuxtVersion(cwd)
-
-        // Check for Module Compatibility
-        if (!checkNuxtCompatibility(matchedModule, nuxtVersion)) {
+    // Match corresponding version of module for local Nuxt version
+    const versionMap = matchedModule.compatibility.versionMap
+    if (versionMap) {
+      for (const [_nuxtVersion, _moduleVersion] of Object.entries(versionMap)) {
+        if (satisfies(nuxtVersion, _nuxtVersion)) {
+          if (!pkgVersion) {
+            pkgVersion = _moduleVersion
+          } else {
             consola.warn(
-                `The module \`${pkgName}\` is not compatible with Nuxt \`${nuxtVersion}\` (requires \`${matchedModule.compatibility.nuxt}\`)`,
+              `Recommended version of \`${pkgName}\` for Nuxt \`${nuxtVersion}\` is \`${_moduleVersion}\` but you have requested \`${pkgVersion}\``,
             )
-            const shouldContinue = await consola.prompt(
-                'Do you want to continue installing incompatible version?',
-                {
-                    type: 'confirm',
-                    initial: false,
-                },
-            )
-            if (shouldContinue !== true) {
-                return false
-            }
+            pkgVersion = await consola.prompt('Choose a version:', {
+              type: 'select',
+              options: [_moduleVersion, pkgVersion],
+            })
+          }
+          break
         }
-
-        // Match corresponding version of module for local Nuxt version
-        const versionMap = matchedModule.compatibility.versionMap
-        if (versionMap) {
-            for (const [_nuxtVersion, _moduleVersion] of Object.entries(versionMap)) {
-                if (satisfies(nuxtVersion, _nuxtVersion)) {
-                    if (!pkgVersion) {
-                        pkgVersion = _moduleVersion
-                    } else {
-                        consola.warn(
-                            `Recommended version of \`${pkgName}\` for Nuxt \`${nuxtVersion}\` is \`${_moduleVersion}\` but you have requested \`${pkgVersion}\``,
-                        )
-                        pkgVersion = await consola.prompt('Choose a version:', {
-                            type: 'select',
-                            options: [_moduleVersion, pkgVersion],
-                        })
-                    }
-                    break
-                }
-            }
-        }
+      }
     }
+  }
 
-    return {
-        nuxtModule: matchedModule,
-        pkg: `${pkgName}@${pkgVersion || 'latest'}`,
-        pkgName,
-        pkgVersion: pkgVersion || 'latest',
-    }
+  return {
+    nuxtModule: matchedModule,
+    pkg: `${pkgName}@${pkgVersion || 'latest'}`,
+    pkgName,
+    pkgVersion: pkgVersion || 'latest',
+  }
 }
