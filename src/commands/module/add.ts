672c53ed76105b53adf85c5f8b2f2dbb31e8e037
rohrig <richard.t.rohrig@gmail.com>
2023-09-26 22:56:59 +0200

    feat: add multiple modules at once

--- src/commands/module/add.ts
+++ src/commands/module/add.ts
@@ -28,46 +28,49 @@ export default defineCommand({
   },
   async setup(ctx) {
     const cwd = resolve(ctx.args.cwd || '.')
+    const modules = ctx.args._
 
-    // TODO: Resolve and validate npm package name first
-    const npmPackage = ctx.args.moduleName
-
-    // Add npm dependency
-    if (!ctx.args.skipInstall) {
-      consola.info(`Installing dev dependency \`${npmPackage}\``)
-      await addDependency(npmPackage, { cwd, dev: true }).catch((err) => {
-        consola.error(err)
-        consola.error(
-          `Please manually install \`${npmPackage}\` as a dev dependency`,
-        )
-      })
-    }
-
-    // Update nuxt.config.ts
-    if (!ctx.args.skipConfig) {
-      await updateNuxtConfig(cwd, (config) => {
-        if (!config.modules) {
-          config.modules = []
-        }
-        for (let i = 0; i < config.modules.length; i++) {
-          if (config.modules[i] === npmPackage) {
-            consola.info(`\`${npmPackage}\` is already in the \`modules\``)
-            return
-          }
-        }
-        consola.info(`Adding \`${npmPackage}\` to the \`modules\``)
-        config.modules.push(npmPackage)
-      }).catch((err) => {
-        consola.error(err)
-        consola.error(
-          `Please manually add \`${npmPackage}\` to the \`modules\` in \`nuxt.config.ts\``,
-        )
-      })
+    for (const moduleName of modules) {
+      // TODO: Resolve and validate npm package name first
+      await addModule(ctx, cwd, moduleName)
     }
   },
 })
 
 // -- Internal Utils --
+async function addModule(ctx, cwd: string, npmPackage: string) {
+  // Add npm dependency
+  if (!ctx.args.skipInstall) {
+    consola.info(`Installing dev dependency \`${npmPackage}\``)
+    await addDependency(npmPackage, { cwd, dev: true }).catch((err) => {
+      consola.error(err)
+      consola.error(
+        `Please manually install \`${npmPackage}\` as a dev dependency`,
+      )
+    })
+  }
+
+  if (!ctx.args.skipConfig) {
+    await updateNuxtConfig(cwd, (config) => {
+      if (!config.modules) {
+        config.modules = []
+      }
+      for (let i = 0; i < config.modules.length; i++) {
+        if (config.modules[i] === npmPackage) {
+          consola.info(`\`${npmPackage}\` is already in the \`modules\``)
+          return
+        }
+      }
+      consola.info(`Adding \`${npmPackage}\` to the \`modules\``)
+      config.modules.push(npmPackage)
+    }).catch((err) => {
+      consola.error(err)
+      consola.error(
+        `Please manually add \`${npmPackage}\` to the \`modules\` in \`nuxt.config.ts\``,
+      )
+    })
+  }
+}
 
 async function updateNuxtConfig(
   rootDir: string,
