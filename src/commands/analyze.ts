b29de196a8e6a55f903078772c488bb0ea581322
Pooya Parsa <pooya@pi0.io>
2023-11-03 14:46:49 +0100

    fix(analyze): set path for client analyze results (#277)

--- src/commands/analyze.ts
+++ src/commands/analyze.ts
@@ -57,6 +57,9 @@ export default defineCommand({
     const outDir =
       nuxt.options.nitro.output?.dir || join(nuxt.options.rootDir, '.output')
 
+    nuxt.options.build.analyze.filename =
+      nuxt.options.build.analyze.filename || join(analyzeDir, 'client.html')
+
     await clearDir(analyzeDir)
     await buildNuxt(nuxt)
 
