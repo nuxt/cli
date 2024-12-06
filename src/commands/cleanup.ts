2f1f6a3563f996acdff63830e5edb6d529c8c6da
Bobbie Goede <bobbiegoede@gmail.com>
2024-12-07 00:22:34 +0100

    fix: load dev config

--- src/commands/cleanup.ts
+++ src/commands/cleanup.ts
@@ -17,7 +17,7 @@ export default defineCommand({
   async run(ctx) {
     const cwd = resolve(ctx.args.cwd || ctx.args.rootDir || '.')
     const { loadNuxtConfig } = await loadKit(cwd)
-    const nuxtOptions = await loadNuxtConfig({ cwd })
+    const nuxtOptions = await loadNuxtConfig({ cwd, overrides: { dev: true } })
     await cleanupNuxtDirs(nuxtOptions.rootDir, nuxtOptions.buildDir)
   },
 })
--- src/utils/nuxt.ts
+++ src/utils/nuxt.ts
@@ -21,7 +21,6 @@ export async function cleanupNuxtDirs(rootDir: string, buildDir: string) {
   await rmRecursive(
     [
       buildDir,
-      '.nuxt',
       '.output',
       'dist',
       'node_modules/.vite',
