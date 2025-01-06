bb384c9382c70f3adcdc8792dc3907bbd1034394
Damian Glowala <damian.glowala.rebkow@gmail.com>
2025-01-06 21:05:34 +0100

    fix: use correct casing utils and simplify usage

--- src/utils/templates/composable.ts
+++ src/utils/templates/composable.ts
@@ -1,17 +1,17 @@
 import type { Template } from '.'
 import { resolve } from 'pathe'
-import { camelCase, upperFirst } from 'scule'
+import { pascalCase } from 'scule'
 
 const composable: Template = ({ name, nuxtOptions }) => {
   const nameWithoutUsePrefix = name.replace(/^use-?/, '')
-  const nameWithUsePrefix = `use${upperFirst(camelCase(nameWithoutUsePrefix))}`
+  const nameWithUsePrefix = `use${pascalCase(nameWithoutUsePrefix)}`
 
   return {
     path: resolve(nuxtOptions.srcDir, `composables/${name}.ts`),
     contents: `
-  export const ${nameWithUsePrefix} = () => {
-    return ref()
-  }
+export const ${nameWithUsePrefix} = () => {
+  return ref()
+}
     `,
   }
 }
--- src/utils/templates/server-util.ts
+++ src/utils/templates/server-util.ts
@@ -1,13 +1,13 @@
 import type { Template } from '.'
 import { resolve } from 'pathe'
-import { pascalCase } from 'scule'
+import { camelCase } from 'scule'
 
 const serverUtil: Template = ({ name, nuxtOptions }) => ({
   path: nuxtOptions.future.compatibilityVersion === 3
     ? resolve(nuxtOptions.srcDir, nuxtOptions.serverDir, 'utils', `${name}.ts`)
     : resolve(nuxtOptions.serverDir, 'utils', `${name}.ts`),
   contents: `
-export function ${pascalCase(name)}() {}
+export function ${camelCase(name)}() {}
 `,
 })
 
