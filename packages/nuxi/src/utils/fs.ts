e8a5dbcc19cc1557f793f1ab69876a565894aa3f
Robin <robin.kehl@singular-it.de>
2025-01-20 12:02:53 +0000

    chore: remove unused fs function

--- packages/nuxi/src/utils/fs.ts
+++ packages/nuxi/src/utils/fs.ts
@@ -3,17 +3,6 @@ import { join } from 'pathe'
 
 import { logger } from '../utils/logger'
 
-// Check if a file exists
-async function exists(path: string) {
-  try {
-    await fsp.access(path)
-    return true
-  }
-  catch {
-    return false
-  }
-}
-
 export async function clearDir(path: string, exclude?: string[]) {
   if (!exclude) {
     await fsp.rm(path, { recursive: true, force: true })
