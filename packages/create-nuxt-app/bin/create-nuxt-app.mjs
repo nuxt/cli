36c920cc8c8d54c8dea59260281f83ac7c9796db
Daniel Roe <daniel@roe.dev>
2025-02-13 22:13:37 +0100

    chore: add `create-nuxt` distribution (#744)

--- packages/create-nuxt-app/bin/create-nuxt-app.mjs
+++ packages/create-nuxt-app/bin/create-nuxt-app.mjs
@@ -1,11 +1,2 @@
 #!/usr/bin/env node
-
-import { fileURLToPath } from 'node:url'
-import { runMain } from '../dist/index.mjs'
-
-globalThis.__nuxt_cli__ = {
-  startTime: Date.now(),
-  entry: fileURLToPath(import.meta.url),
-}
-
-runMain()
+import 'create-nuxt/cli'
