23d296dd1e4a9835961c6213e8692d5cbd41fbf9
Daniel Roe <daniel@roe.dev>
2025-06-04 16:14:55 +0200

    test: add dev benchmark

---
+++ packages/nuxt-cli/test/bench/dev.bench.ts
@@ -0,0 +1,29 @@
+import { rm } from 'node:fs/promises'
+import { join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import { bench, describe } from 'vitest'
+
+import { runCommand } from '../../../nuxi/src/run'
+
+describe('dev', async () => {
+  const fixtureDir = fileURLToPath(new URL('../../playground', import.meta.url))
+  await rm(join(fixtureDir, '.nuxt'), { recursive: true, force: true })
+
+  bench('starts dev server', async () => {
+    const { result } = await runCommand('dev', [fixtureDir], {
+      overrides: {
+        builder: {
+          bundle: (nuxt) => {
+            nuxt.hooks.removeAllHooks()
+            return Promise.resolve()
+          },
+        },
+      },
+    })
+    await (result as { listener: any }).listener.close()
+  })
+
+  // it.skip('makes requests to dev server', async () => {
+  // })
+})
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
+import codspeed from '@codspeed/vitest-plugin'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  plugins: [codspeed()],
   test: {
     coverage: {},
   },
