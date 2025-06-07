59f83b1211a8e137453b5d0d6264b5e85727c708
Daniel Roe <daniel@roe.dev>
2025-06-07 23:01:31 +0200

    test: close servers (#904)

--- packages/nuxt-cli/test/bench/dev.bench.ts
+++ packages/nuxt-cli/test/bench/dev.bench.ts
@@ -1,34 +1,20 @@
 import type { Nuxt } from '@nuxt/schema'
 import type { Listener } from 'listhen'
 
-import { rm } from 'node:fs/promises'
 import os from 'node:os'
-import { join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
+import { runCommand } from '@nuxt/cli'
 import { bench, describe } from 'vitest'
 
-import { runCommand } from '../../../nuxi/src/run'
+interface RunResult {
+  result: { listener: Listener, close: () => Promise<void> }
+}
 
-describe(`dev [${os.platform()}]`, async () => {
-  const fixtureDir = fileURLToPath(new URL('../../playground', import.meta.url))
-  await rm(join(fixtureDir, '.nuxt'), { recursive: true, force: true })
+const fixtureDir = fileURLToPath(new URL('../../playground', import.meta.url))
 
-  bench('starts dev server', async () => {
-    const { result } = await runCommand('dev', [fixtureDir, '--fork'], {
-      overrides: {
-        builder: {
-          bundle: (nuxt: Nuxt) => {
-            nuxt.hooks.removeAllHooks()
-            return Promise.resolve()
-          },
-        },
-      },
-    }) as { result: { listener: Listener } }
-    await result.listener.close()
-  })
-
-  bench('starts dev server in no-fork mode', async () => {
+describe(`dev [${os.platform()}]`, () => {
+  bench(`starts dev server with --no-fork`, async () => {
     const { result } = await runCommand('dev', [fixtureDir, '--no-fork'], {
       overrides: {
         builder: {
@@ -38,18 +24,23 @@ describe(`dev [${os.platform()}]`, async () => {
           },
         },
       },
-    }) as { result: { listener: Listener } }
-    await result.listener.close()
+    }) as RunResult
+    await result.close()
   })
 
-  const { result } = await runCommand('dev', [fixtureDir]) as { result: { listener: Listener } }
-  const url = result.listener.url
-
+  let url: string
   bench('makes requests to dev server', async () => {
+    if (!url) {
+      const { result } = await runCommand('dev', [fixtureDir, '--no-fork']) as RunResult
+      url = result.listener.url
+    }
     const html = await fetch(url).then(r => r.text())
     if (!html.includes('Welcome to the Nuxt CLI playground!')) {
       throw new Error('Unexpected response from dev server')
     }
     await fetch(`${url}_nuxt/@vite/client`).then(r => r.text())
-  }, { time: 10_000 })
+  }, {
+    warmupIterations: 1,
+    time: 10_000,
+  })
 })
