29ddb605d4b9f0414ab2e245f77ce594c61c3a26
Daniel Roe <daniel@roe.dev>
2025-06-15 21:01:16 +0100

    fix: correctly polyfill `globalThis.crypto`
    https://github.com/nitrojs/nitro/pull/3402

--- packages/nuxi/src/main.ts
+++ packages/nuxi/src/main.ts
@@ -14,7 +14,7 @@ import { logger } from './utils/logger'
 
 // globalThis.crypto support for Node.js 18
 if (!globalThis.crypto) {
-  globalThis.crypto = nodeCrypto as unknown as Crypto
+  globalThis.crypto = nodeCrypto.webcrypto as unknown as Crypto
 }
 
 export const main = defineCommand({
