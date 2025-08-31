97d4efdeb1cde87c318f53f7bf8c1968984c7689
Oskar Lebuda <oskar.lebuda@enp.pl>
2025-08-31 21:27:14 +0200

    feat(build,dev): add every bundler version

--- packages/nuxi/src/utils/banner.ts
+++ packages/nuxi/src/utils/banner.ts
@@ -3,35 +3,59 @@ import { readFileSync } from 'node:fs'
 import { colors } from 'consola/utils'
 import { resolveModulePath } from 'exsolve'
 
-import { tryResolveNuxt } from './kit'
+import { loadKit, tryResolveNuxt } from './kit'
 import { logger } from './logger'
 
-export function showVersions(cwd: string) {
+export async function showVersions(cwd: string) {
   const { bold, gray, green } = colors
   const nuxtDir = tryResolveNuxt(cwd)
-  function getPkgVersion(pkg: string) {
+
+  const kit = await loadKit(cwd)
+  const config = await kit.loadNuxtConfig({ cwd })
+
+  function getBuilder(): { name: string, version: string } {
+    switch (config.builder) {
+      case '@nuxt/rspack-builder':
+        return { name: 'Rspack', version: getPkgVersion('@rspack/core') }
+      case '@nuxt/webpack-builder':
+        return { name: 'Webpack', version: getPkgVersion('webpack') }
+      case '@nuxt/vite-builder':
+      default: {
+        const pkgJSON = getPkgJSON('vite')
+        const isRolldown = pkgJSON.name.includes('rolldown')
+        return { name: isRolldown ? 'Rolldown-Vite' : 'Vite', version: pkgJSON.version }
+      }
+    }
+  }
+
+  function getPkgJSON(pkg: string) {
     for (const url of [cwd, nuxtDir]) {
       if (!url) {
         continue
       }
       const p = resolveModulePath(`${pkg}/package.json`, { from: url, try: true })
       if (p) {
-        return JSON.parse(readFileSync(p, 'utf-8')).version as string
+        return JSON.parse(readFileSync(p, 'utf-8'))
       }
     }
-    return ''
+    return null
   }
+
+  function getPkgVersion(pkg: string) {
+    const pkgJSON = getPkgJSON(pkg)
+    return pkgJSON?.version ?? ''
+  }
+
   const nuxtVersion = getPkgVersion('nuxt') || getPkgVersion('nuxt-nightly') || getPkgVersion('nuxt3') || getPkgVersion('nuxt-edge')
   const nitroVersion = getPkgVersion('nitropack') || getPkgVersion('nitropack-nightly') || getPkgVersion('nitropack-edge')
-  const viteVersion = getPkgVersion('rolldown-vite') || getPkgVersion('vite') || null
-  const isRolldown = viteVersion?.includes('rolldown')
+  const builder = getBuilder()
   const vueVersion = getPkgVersion('vue') || null
 
   logger.log(
     green(`Nuxt ${bold(nuxtVersion)}`)
     + gray(' (with: ')
     + (nitroVersion ? gray(`Nitro ${bold(nitroVersion)}`) : '')
-    + (viteVersion ? gray(`, ${isRolldown ? 'Rolldown-Vite' : 'Vite'} ${bold(viteVersion)}`) : '')
+    + gray(`, ${builder.name} ${bold(builder.version)}`)
     + (vueVersion ? gray(`, Vue ${bold(vueVersion)}`) : '')
     + gray(')'),
   )
