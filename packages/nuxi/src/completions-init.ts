fd1747ffb46973bf83ac97c3c88e375a66c08989
AmirHossein Sakhravi <amirhosseinpr184@gmail.com>
2025-11-04 20:46:22 +0330

    feat: add shell completions using `@bomb.sh/tab` (#1082)

---
+++ packages/nuxi/src/completions-init.ts
@@ -0,0 +1,27 @@
+import type { ArgsDef, CommandDef } from 'citty'
+import tab from '@bomb.sh/tab/citty'
+import { templates } from './utils/completions-data'
+
+export async function setupInitCompletions<T extends ArgsDef = ArgsDef>(command: CommandDef<T>) {
+  const completion = await tab(command)
+
+  const templateOption = completion.options?.get('template')
+  if (templateOption) {
+    templateOption.handler = (complete) => {
+      for (const template of templates) {
+        complete(template, '')
+      }
+    }
+  }
+
+  const logLevelOption = completion.options?.get('logLevel')
+  if (logLevelOption) {
+    logLevelOption.handler = (complete) => {
+      complete('silent', 'No logs')
+      complete('info', 'Standard logging')
+      complete('verbose', 'Detailed logging')
+    }
+  }
+
+  return completion
+}
