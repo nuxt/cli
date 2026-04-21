a8ef670a74ae41d3466b2af8c57f01c2caa36409
Daniel Roe <daniel@roe.dev>
2026-04-21 13:16:13 +0100

    test: remove test about throwing

--- packages/nuxi/test/unit/utils/banner.spec.ts
+++ packages/nuxi/test/unit/utils/banner.spec.ts
@@ -21,12 +21,6 @@ vi.mock('../../../src/utils/versions', () => ({
 const { getBuilder } = await import('../../../src/utils/banner')
 
 describe('getBuilder', () => {
-  it('does not throw when vite package.json cannot be resolved', async () => {
-    const { getPkgJSON } = await import('../../../src/utils/versions')
-    vi.mocked(getPkgJSON).mockReturnValueOnce(null)
-    expect(() => getBuilder('/any', 'vite')).not.toThrow()
-  })
-
   it('resolves vite version via nuxt -> @nuxt/vite-builder', () => {
     expect(getBuilder('/any', 'vite')).toEqual({ name: 'Vite', version: '7.3.1' })
   })
