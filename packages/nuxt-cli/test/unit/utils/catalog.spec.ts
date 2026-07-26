059bc38814a38b1a1b4fef7eb775ae85266a14e3
Daniel Roe <daniel@roe.dev>
2026-07-26 15:13:02 +0100

    test: use `pathe`

--- packages/nuxt-cli/test/unit/utils/catalog.spec.ts
+++ packages/nuxt-cli/test/unit/utils/catalog.spec.ts
@@ -1,7 +1,7 @@
 import { mkdir, mkdtemp, rm, writeFile } from 'node:fs/promises'
 import { tmpdir } from 'node:os'
-import { join } from 'node:path'
 
+import { join } from 'pathe'
 import { afterEach, beforeEach, describe, expect, it } from 'vitest'
 
 import { findPnpmWorkspaceYaml, parseCatalogSpecifier, readCatalogConfig, resolveCatalogEntry } from '../../../src/utils/catalog'
