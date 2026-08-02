634eeb78b293de328a4ef65dcad883562dc640e0
Daniel Roe <daniel@roe.dev>
2026-08-02 13:44:42 +0000

    fix(cli): align Node warning with package engines

--- packages/nuxt-cli/src/utils/engines.ts
+++ packages/nuxt-cli/src/utils/engines.ts
@@ -2,15 +2,15 @@ import process from 'node:process'
 import { styleText } from 'node:util'
 import { satisfies } from 'verkit'
 
+import { engines } from '../../package.json'
 import { logger } from './logger'
 
 export async function checkEngines() {
   const currentNode = process.versions.node
-  const nodeRange = '>= 18.0.0'
 
-  if (!satisfies(currentNode, nodeRange)) {
+  if (!satisfies(currentNode, engines.node)) {
     logger.warn(
-      `Current version of Node.js (${styleText('cyan', currentNode)}) is unsupported and might cause issues.\n       Please upgrade to a compatible version ${styleText('cyan', nodeRange)}.`,
+      `Current version of Node.js (${styleText('cyan', currentNode)}) is unsupported and might cause issues.\n       Please upgrade to a compatible version ${styleText('cyan', engines.node)}.`,
     )
   }
 }
